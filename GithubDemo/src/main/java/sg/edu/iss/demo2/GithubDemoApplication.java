3c9482daf72c0468c839bd23127567375a0d2bad
Cai Yaolong Derek <44289562+caiylderek@users.noreply.github.com>
2020-12-07 16:10:36 +0800

    First commit or something

---
+++ GithubDemo/src/main/java/sg/edu/iss/demo2/GithubDemoApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDemoApplication.class, args);
+	}
+
+}
---
+++ GithubDemo/src/test/java/sg/edu/iss/demo2/GithubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
